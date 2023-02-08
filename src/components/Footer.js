1cc646173ef21bf1e45ee66aa43094e1dade68f2
A Stone <simplererstone@gmail.com>
2023-02-08 16:58:34 -0500

    little change on anchors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,4 @@
 import React from "react";
-import styled from "styled-components";
-
-const AnchorLink = styled.a`
-   text-decoration: none;
-   cursor: pointer;
- `;
 
 function Footer() {
 
@@ -12,12 +6,12 @@ function Footer() {
     <div className="pt-3">
       <div className="card text-center">       
         <div className="card-body d-flex justify-content-center">
-        <AnchorLink href="https://www.linkedin.com/in/scott-stone-392156231/" target={"_blank"}>
-          <i className="fa fa-linkedin-square fa-3x p-2 me-2" aria-hidden="true"></i></AnchorLink>
-        <AnchorLink href="https://github.com/Simplerer" target={"_blank"}>
-          <i className="fa fa-github fa-3x p2 m-2" aria-hidden="true"></i></AnchorLink>
-        <AnchorLink href="https://stackoverflow.com/users/21167977/simplerer" target={"_blank"}>
-          <i className="fa fa-stack-overflow fa-3x p-2 ms-2" aria-hidden="true"></i></AnchorLink>        
+        <a href="https://www.linkedin.com/in/scott-stone-392156231/" target={"_blank"}>
+          <i className="fa fa-linkedin-square fa-3x p-2 me-2" aria-hidden="true"></i></a>
+        <a href="https://github.com/Simplerer" target={"_blank"}>
+          <i className="fa fa-github fa-3x p2 m-2" aria-hidden="true"></i></a>
+        <a href="https://stackoverflow.com/users/21167977/simplerer" target={"_blank"}>
+          <i className="fa fa-stack-overflow fa-3x p-2 ms-2" aria-hidden="true"></i></a>        
         </div>
       </div>
     </div>
