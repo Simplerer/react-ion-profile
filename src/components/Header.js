d59ef83211cc0eda281fcccb5ff68414fa61f653
A Stone <simplererstone@gmail.com>
2023-02-10 10:47:41 -0500

    cleaned up header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,37 +2,6 @@ import React from "react";
 
 function Header({ pageChanger, currentPage }) {
   return (
-    // <div className="pt-3">
-    //   <div className="card">
-    //     <div className="card-body">
-    //       <div className="col">
-    //         <h1>Scott Stone</h1>
-    //       </div>
-    //     </div>
-    //     <div className="card-header">
-    //       <ul className="nav nav-tabs card-header-tabs justify-content-end">
-    //         <li className="nav-item">
-    //           <a className={currentPage == 'About' ? 'nav-link-active' : 'nav-link'}
-    //             href="#about"
-    //             onClick={() => pageChanger('About')}>About</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className={currentPage == 'Portfolio' ? 'nav-link-active' : 'nav-link'}
-    //             href='#portfolio'
-    //             onClick={() => pageChanger('Portfolio')}>Portfolio</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className={currentPage == 'Contact' ? 'nav-link-active' : 'nav-link'}
-    //             href="#contact"
-    //             onClick={() => pageChanger('Contact')}>Artistic</a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="#">Artistic</a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="container">
       <div>
 
